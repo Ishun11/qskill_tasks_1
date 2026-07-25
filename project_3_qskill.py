1c0d5f93f8dacee64e3a537c462672a68ff46503
ishan sonel <ishansonel1106@gmail.com>
2026-07-25 23:58:28 +0530

    project_3_qskill.py

--- project_3_qskill.py
+++ project_3_qskill.py
@@ -1,10 +1,8 @@
 from groq import Groq
 messages = []
-while True:
  text = input("enter a text : ")
  if text == 'exit':
   print("okk byy")
-  break
 
  client = Groq(api_key="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")
  chat = client.chat.completions.create(
